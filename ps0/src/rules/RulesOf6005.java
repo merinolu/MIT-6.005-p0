8bb63bfdbe5475e8efd32ca881df1629d7a2a818
Luis Merino <luis.pablo.merino.m@gmail.com>
2022-11-25 23:03:16 -0500

    Initial creation

--- ps0/src/rules/RulesOf6005.java
+++ ps0/src/rules/RulesOf6005.java
@@ -38,7 +38,7 @@ public class RulesOf6005 {
         
         // TODO: Fill in this method, then remove the exception
         
-        throw new RuntimeException("implement me!");
+        return writtenByYourself || (availableToOthers && !writtenAsCourseWork && citingYourSource && !implementationRequired);
     }
     
     /**
